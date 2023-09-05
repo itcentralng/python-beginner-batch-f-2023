52185531f818281cba7ec394967685ab22570586
Nasir Mustapha <teboy.com@gmail.com>
2023-09-05 12:46:43 +0100

    first assigment

---
+++ fundamentals/assignments/string.py
@@ -0,0 +1,8 @@
+# 1. Write a short description of what you understand a string
+# to be, in your own words.
+
+# 2. Give 10 examples of strings
+
+# What do you understand by a variable?
+
+# 4. Give 5 examples of bad variables names
