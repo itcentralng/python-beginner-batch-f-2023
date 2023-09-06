2517497b5f94de4cf6d9ad5e15366b049da79f67
Zakiyya <zakiyyasaid24@gmail.com>
2023-09-06 08:49:23 -0700

    datatypes.py

---
+++ zakiyya/classwork/datatypes.py
@@ -0,0 +1,42 @@
+# write 5 examples for each of the following
+# - List
+names = ['amina','suwaiba']
+emotions = ['happiness','achievement'['satisfaction','pleasure','depression'] 'certainty']
+fruits = ['mango', 'strawberry', 'cashew', 'watermelon']
+clothes = ['blouse', 'skirt', 'shirt','trousers']
+places = ['market', 'houses',['schools', 'teachers','students']'mosques','churches']
+
+# - Tuple
+names = ('muhsina','ibrahim')
+clothing = ('necklace', 'earrings','rings'['dress', 'hats','headtie','shoes'])
+stationaries = ('books','pencils', 'erasers','pen')
+sex = ('boys','girls'['man','woman'] 'sister','brother')
+gadget = ('phone','laptop','iphone','ipad')
+
+# - dictionary
+{'name':'amina','khadija', 'age':16, 'height:13.5'}
+{'clothes':'blouse', 'dress'['shoes':'halfCover','flat','highHill']}
+{'fruits':'orange','pineapple','strawberry','grape','pear'}
+{'tribe':'hausa','yoruba','igbo'['religion':'islam','christian']}
+{'places':'market','schools',['drugs':'paracetamol','artemether','diclofenac']}
+
+# - Integer
+age = 30
+height = 59
+weight = 29
+number = 1,2,5,8
+length = 34, 16
+
+# = Float
+age = 32.4
+height = 45.8
+weight = 23.6
+number = 667.9
+length = 17.4
+
+# - Boolean
+sheIsHeartless = True 
+theyAreHappy = True
+heIsHungry = False
+aliIsDrowning = False
+sheWasCaptured = True
