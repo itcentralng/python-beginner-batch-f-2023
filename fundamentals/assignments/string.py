8a1910362fc1d0de8bc2aeeb41a3ee5db63ccf82
Nasir Mustapha <teboy.com@gmail.com>
2023-09-07 11:04:46 +0100

    string methods and assingment

--- fundamentals/assignments/string.py
+++ fundamentals/assignments/string.py
@@ -1,3 +1,4 @@
+# Assignment 1
 # 1. Write a short description of what you understand a string
 # to be, in your own words.
 
@@ -6,3 +7,18 @@
 # What do you understand by a variable?
 
 # 4. Give 5 examples of bad variables names
+
+# Assignment 2
+# 1. For each of the following methods, explain what they do with 2 examples:
+# - startswith
+# - format
+# - index
+# - lower
+# - upper
+# - isalpha
+# - isalnum
+# - islower
+# - isupper
+# - replace
+# - split
+# - strip
--- fundamentals/datatypes/string.py
+++ fundamentals/datatypes/string.py
@@ -7,6 +7,20 @@ name2 = 'Usman'
 
 name3 = """Yusuf"""
 
+name4 = "mohammad"
+
+x = "I am A SMALL Driver"
+
 # METHODS
+print(name4.capitalize())
+
+print(x.casefold())
+
+print(name3.center(7, '*'))
+
+print(name4.count('x', 0, 6))
+
+
+print(name4.endswith('d'))
 
 # OPERATIONS
