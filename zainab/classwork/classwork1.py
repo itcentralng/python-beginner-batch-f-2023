0c5f503dcc268dc44d88dd7d5da95f948fc4bf3b
nabusani <144116907+nabusani@users.noreply.github.com>
2023-09-11 05:29:34 -0700

    classwork2

---
+++ zainab/classwork/classwork1.py
@@ -0,0 +1,36 @@
+names=["maryam","ebube","zakari"("jogging,"strolling")]
+places=["abuja","holiday","enjoy"]
+subjects["practicals","chemistry"]
+items=[("utensils","gadgets")]
+things["drinks","snacks"("cars","bikes")]
+
+movies=("madman","encounter")
+country=("education","mexico","peakyblinders")
+website=("youtube")
+subject=("maths","english")
+("houseofdragons")
+
+{"project:robotcreation"({"name:zainab"})"movies:bluebeetle"}
+{"peace:nigeria"}
+{"university:kasu"("language:chinese")}
+{("team:chelsea")("country:bacelona")}
+{("names:aisha,zakiyya,abdullahi,shedrack")}
+
+
+numbers= 123
+randomnum= 2337
+matrices= +023552
+index= 00355
+evennum= 246810
+
+decimals= 0.7352
+logrithm= -89.086
+calculus= 628.0008
+numbers= 123.123
+odddecimals= -13.57
+
+iWentToSchool=True
+niceBreakFast=True
+takeABreak=False
+flipFlops=True
+iLikeSchool=False
---
+++ zainab/string.py
@@ -0,0 +1,17 @@
+A string is a sequence of characters like for example letters numbers.
+
+ "hello, people"
+ "I am awesome"
+ "127637"
+ "instagram"
+ "the history of gowon"
+ "coding is fun"
+ "tolu, chibuzo, isabella"
+ "i might like food"
+ "45"
+ "The managing director,15,calm down"
+
+A variable like a named storage location.its can also be assigned multiple and different values during 
+during the process of programming.
+
+ 
