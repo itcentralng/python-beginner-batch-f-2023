17f2d2191b396a15887f2c64627e713da8edae11
Zakiyya <zakiyyasaid24@gmail.com>
2023-09-13 04:52:35 -0700

    list.py

---
+++ zakiyya/classwork/assignment.py
@@ -0,0 +1,68 @@
+
+
+# Assignment 2
+# 1. For each of the following methods, explain what they do with 2 examples:
+# - startswith
+
+# it shows true if the string starts with the specified value
+name= "aliyah"
+job= "teaching"
+house ="duplex"
+emotion ="happiness"
+clothing ="shirt"
+
+print(name.startswith("a"))
+print(job.startswith("t"))
+print(house.startswith("d"))
+print(emotion.startswith("h"))
+print(clothing.startswith("s"))
+
+# - format
+
+# it format specified value or values in a string
+name = "hi am {khadija}"
+emotion = "it was {fantastic}"
+
+print(name.format(khadija="zakiyya"))
+print(emotion.format(fantastic="awesome"))
+
+# - index
+
+# it searches the string of a specified value and inform us the position of the value
+name = "hi am zakiyya"
+emotion = "it was awesome"
+
+print(name.index("z"))
+print(emotion.index("o"))
+
+# - lower
+
+# it converts a string into lower case
+job = "lecturer"
+hobby = "reading"
+
+print(job.lower())
+print(hobby.lower())
+
+# - upper
+
+# it converts a string into upper case
+job = "lecturer"
+hobby = "reading"
+
+print(job.upper())
+print(hobby.upper())
+
+# - isalpha
+
+# it returns true if all characters in the string are in alphabet
+# - isalnum
+# - islower
+# - isupper
+# - replace
+# - split
+# - strip# 
+
+
+
+
