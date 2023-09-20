ecae3a4a97d73d35826558ceaa662055b1b9d346
Nasir Mustapha <teboy.com@gmail.com>
2023-09-20 10:30:25 +0100

    tuples and their methods

--- fundamentals/datatypes/tuple.py
+++ fundamentals/datatypes/tuple.py
@@ -2,4 +2,15 @@ people = ("James", "Mariya", "Cynthia", ["Abubakar", "Ibrahim", [("Musa", "Saleh
 
 # METHODS
 
+print(people.index('James'))
+print(people.count('James'))
+
 # OPERATIONS
+print(people[:])
+print(people[:3])
+print(people[::-1])
+print(people)
+
+numbers = 1, 2, 3, 4
+
+print(numbers.index(2))
