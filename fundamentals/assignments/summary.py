4d17709e8fc7fb5f36a1bcc4517a513fe525c6b7
Nasir Mustapha <teboy.com@gmail.com>
2023-09-20 11:50:35 +0100

    summary assignment

---
+++ fundamentals/assignments/summary.py
@@ -0,0 +1,65 @@
+"""
+**Strings:**
+1. What are the different ways to create a string in Python?
+2. How do you get the length of a string?
+3. How do you split a string into a list of words?
+"""
+# Write your answers here:
+
+"""
+**Lists:**
+1. How do you create a list in Python?
+2. How do you add an item to a list?
+3. How do you remove an item from a list?
+"""
+# Write your answers here:
+
+"""
+**Tuples:**
+1. What is the difference between a list and a tuple?
+2. How do you create a tuple in Python?
+3. How do you access an item in a tuple?
+"""
+# Write your answers here:
+
+"""
+**Dictionaries:**
+1. How do you create a dictionary in Python?
+2. How do you add a key-value pair to a dictionary?
+3. How do you get the value for a given key in a dictionary?
+"""
+# Write your answers here:
+
+"""
+Practice questions:
+
+**Strings:**
+1. Write a Python program to reverse a string.
+2. Write a Python program to check if a string is a palindrome.
+3. Write a Python program to find the most frequent character in a string.
+"""
+# Write your answers here:
+
+"""
+**Lists:**
+1. Write a Python program to find the largest number in a list.
+2. Write a Python program to find the sum of all the elements in a list.
+3. Write a Python program to remove all duplicates from a list.
+"""
+# Write your answers here:
+
+"""
+**Tuples:**
+1. Write a Python program to find the length of a tuple.
+2. Write a Python program to find the smallest number in a tuple.
+3. Write a Python program to sort a tuple in ascending order.
+"""
+# Write your answers here:
+
+"""
+**Dictionaries:**
+1. Write a Python program to find the number of key-value pairs in a dictionary.
+2. Write a Python program to check if a key exists in a dictionary.
+3. Write a Python program to print all the keys in a dictionary.
+"""
+# Write your answers here:
