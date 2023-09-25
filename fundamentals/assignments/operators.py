e61bf004304c1749ca47c0f89683c743bf213d60
Nasir Mustapha <teboy.com@gmail.com>
2023-09-25 13:36:29 +0100

    introduction to operators

---
+++ fundamentals/assignments/operators.py
@@ -0,0 +1,5 @@
+# Answer the followng using only the concepts we've treated in class:
+# Q1. Write a python program that asks a user to guess a number and 
+# you tell them whether they guessed it right or not
+# Q2. Write a python program that asks a user to guess if a certain
+# fruit exists in a supermarket
---
+++ fundamentals/operators/comparison.py
@@ -0,0 +1,27 @@
+# Comparison: from the name, they are used to compare data.
+
+# ==
+num1 = 1
+num2 = 2
+
+name = "Aisha"
+name2 = "aisHA"
+
+print(name.lower() == name2.lower())
+
+print(len(name) > len(name2))
+print(len(name) >= len(name2))
+
+print(num1 == num2)
+
+# >
+print(num1 > num2)
+
+# <
+print(num1 < num2)
+
+# >=
+print(num1>=num2)
+
+# <=
+print(num1<=num2)
