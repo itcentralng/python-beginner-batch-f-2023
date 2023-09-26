4a43f5544073975b70a9d40c8a93ba03203b6589
Nasir Mustapha <teboy.com@gmail.com>
2023-09-26 11:15:37 +0100

    more operator examples

--- fundamentals/operators/assignment.py
+++ fundamentals/operators/assignment.py
@@ -2,6 +2,10 @@
 # =
 name = "Omale"
 
+name += " David"
+
+print(name)
+
 # +=
 num1 = 5
 
@@ -13,3 +17,32 @@ num1 //= 5
 num1 %= 5
 
 print(num1)
+
+
+num1 = 1
+num2 = 2
+
+num1 - num2
+print(num1)
+
+num1 -= num2
+print(num1)
+
+
+num5 = 16
+num6 = 20
+
+print(num5 + 2)
+print(num5)
+num5 += 2
+print(num5)
+
+num1 = 2
+num2 = 3
+
+num2 * num1
+print(num1)
+num2 *= num1
+print(num1)
+
+print(num2 % 2)
