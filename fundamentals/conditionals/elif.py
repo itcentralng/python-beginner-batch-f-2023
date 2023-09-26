7ee1a683d40419c05489ff3ac051b84de3da07e7
Nasir Mustapha <teboy.com@gmail.com>
2023-09-26 12:01:01 +0100

    introduction to conditonal statement

---
+++ fundamentals/conditionals/elif.py
@@ -0,0 +1,6 @@
+classroom = ['Aisha', 'Abdul', 'Omale', 'Alamin', 'Ahmad']
+
+if 'Aisha' in classroom:
+    print('Every takes 1K')
+elif 'Omale' in classroom:
+    print('Every takes 5H')
---
+++ fundamentals/conditionals/else.py
@@ -0,0 +1,14 @@
+classroom = ['Aisha', 'Abdul', 'Alamin', 'Ahmad']
+
+# the if keyword condition colon
+# we indent and add our code to be executed
+if 'Aisha' in classroom:
+    print('Every takes 1K')
+elif 'Omale' in classroom:
+    print('Every takes 5H')
+elif 'Alamin' in classroom:
+    print('Every takes 3H')
+elif 'Ahmad' in classroom:
+    print('Every takes 2H')
+else:
+    print('Every takes 1H')
