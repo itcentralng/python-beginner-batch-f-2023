dc682987fb9792d8bc83b8dc50427ef4580077ea
Nasir Mustapha <teboy.com@gmail.com>
2023-09-28 12:32:45 +0100

    for loop assignment

---
+++ fundamentals/assignments/forloop.py
@@ -0,0 +1,5 @@
+# Q1. Loop through numbers between 1 and 15 inclusive.
+# for each number, if the number is divisible by 3 print "Fizz"
+# if the number is divisible by 5 print "Buzz"
+# if the number is divisible by both 3 and 5 print "FizzBuzz"
+# Otherwise print the number.
