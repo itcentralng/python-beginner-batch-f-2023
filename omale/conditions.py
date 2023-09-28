dac49f5e9b0a9b4d28e7ee468e7698d28747d6fe
duchesne omale enemona <duchesneomaleenemona@gmail.com>
2023-09-28 04:36:38 -0700

    work op

---
+++ omale/conditions.py
@@ -0,0 +1,25 @@
+# Q1. Write a program that simulates the login page of a website.
+# Users will be allowed to login with their email and password.
+# If their credentials are correct, you welcome them by their names.
+# If either of their credentials are wrong, you notify them appropraitely.
+# OPTIONAL: You can ask users to register first.
+# NOTE: Create a database that stores information about users
+# Information about users can be name, email and password or more
+
+# answers
+print('reistration process for zoom')
+registration_name=input('please tell us your name:')
+registration_email=input('enter your email address:')
+registration_password=input('enter your password:')
+reistratiion_phonenmber=input('enter your phone number:')
+
+login_details={'name':'please tells us your name',
+               'email':'enter your email address',
+               'password':'enter your password',
+               'phonenumber':'enter your phone number'}
+print('registration is successful')
+
+
+
+
+
