c1c26860baee93b7eeab0b2b7379ebac026ecca8
Aeesha Aliyy <aeeshaaliyy21@gmail.com>
2023-10-05 12:18:30 +0200

    loop

---
+++ Aisha/Assignments/loop2.py
@@ -0,0 +1,4 @@
+# Write a state and capital game where users are presented with a state for which they provide the capital,
+# if correct the program indicates that and if they are wrong the program corrects them and continues until
+# all states are exhausted or user want to quit.
+
