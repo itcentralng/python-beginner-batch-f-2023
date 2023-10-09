9123144d50f8bfb65f01192f7c4b2d4b62643e44
Nasir Mustapha <teboy.com@gmail.com>
2023-10-09 11:45:57 +0100

    introduction to functions

--- Aisha/Assignments/loop2.py
+++ Aisha/Assignments/loop2.py
@@ -51,61 +51,61 @@
 
 # WHILE IMPLEMENTATION
 # 1
+def startGame():
+    states = [
+        {"name":"Kaduna", "capital":"Kaduna"},
+        {"name":"Kano", "capital":"Kano"},
+        {"name":"Oyo", "capital":"Ibadan"},
+    ]
+    import random
+    score = 0
 
-states = [
-    {"name":"Kaduna", "capital":"Kaduna"},
-    {"name":"Kano", "capital":"Kano"},
-    {"name":"Oyo", "capital":"Ibadan"},
-]
-import random
-score = 0
+    # 1.2
+    print("\n\nWelcome to the state and capital game")
+    print("To quit, enter 'Q' or 'q' at anytime.")
+    print("For every correct answer given, five points are added to your score. \nThree points are removed for every wrong answer given.\n\n")
+    #  
 
-# 1.2
-print("\n\nWelcome to the state and capital game")
-print("To quit, enter 'Q' or 'q' at anytime.")
-print("For every correct answer given, five points are added to your score. \nThree points are removed for every wrong answer given.\n\n")
-#  
-
-playing = True
-usedStates = []
-username = input("Enter username: ")
-while playing:
-    state = random.choice(states)
-    if (state.get('name') in usedStates) == False:
-        capital = input("What is the capital of {}: ".format(state.get('name')))
-        # 3
-        if capital.lower().strip() == 'q':
-            playing = False
-            
-        elif capital.lower().strip() == state.get('capital').lower().strip():
-            # 4
-            print("Correct")
-            usedStates.append(state.get('name'))
-            score += 5
-            print("Your new score is: ", score)
-        else:
-            print("Thats wrong, the capital of {} is {}".format(state.get('name'), state.get('capital')))
-            score -= 3
-            print("Your new score is: ", score)
-    elif len(states) == len(usedStates):
-            playing = False
-            print()
-            print(username, "Your overall score is: ", score)
-            print("Goodbye, you've exhausted our levels")
-            restart = input("Would you like to play again? If yes, Enter 'Yes'. If no, Enter 'Q' or 'q': ").strip().lower()
-            if restart == "yes":
-                playing = True
-                usedStates.clear()
-                score = 0
+    playing = True
+    usedStates = []
+    username = input("Enter username: ")
+    while playing:
+        state = random.choice(states)
+        if (state.get('name') in usedStates) == False:
+            capital = input("What is the capital of {}: ".format(state.get('name')))
+            # 3
+            if capital.lower().strip() == 'q':
+                playing = False
+                
+            elif capital.lower().strip() == state.get('capital').lower().strip():
+                # 4
+                print("Correct")
+                usedStates.append(state.get('name'))
+                score += 5
+                print("Your new score is: ", score)
             else:
-                print(username,",", "Your final score: ", score)
-                print("Goodbye")
-                break
-           
+                print("Thats wrong, the capital of {} is {}".format(state.get('name'), state.get('capital')))
+                score -= 3
+                print("Your new score is: ", score)
+        elif len(states) == len(usedStates):
+                playing = False
+                print()
+                print(username, "Your overall score is: ", score)
+                print("Goodbye, you've exhausted our levels")
+                restart = input("Would you like to play again? If yes, Enter 'Yes'. If no, Enter 'Q' or 'q': ").strip().lower()
+                if restart == "yes":
+                    playing = True
+                    usedStates.clear()
+                    score = 0
+                else:
+                    print(username,",", "Your final score: ", score)
+                    print("Goodbye")
+                    break
+            
 
-               
-            
-            
+                
+                
+                
 
 # Q2. Write a State and Capital Game where users are presented with a state
 # for which they provide the capital, if they are correct the program
---
+++ fundamentals/functions/example2.py
@@ -0,0 +1,10 @@
+from example import send_email
+
+send_email("aisha@gmail.com", "Testing", "This is a test body")
+
+# Q1. Write a function that takes name and age 
+# and print "Hi my name is the-name-goes-here and I am the-age-goes-here years old"
+
+# Q2 Write a function that takes two lists e.g. list1=["name", "age", "height"]
+# list2 = ["Ahmad Sani", 15, 1.6] and 
+# returns a dictionary e.g. {"name":"Ahmad Sani", "age":15, "height":1.6}
