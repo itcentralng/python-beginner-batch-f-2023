c69be8e025ecd838af7b8bfaf32663f21879346c
Ahmad <ahmadatto@gmal.com>
2023-10-09 11:49:27 +0100

    ...

--- fundamentals/assignments/loop.py
+++ fundamentals/assignments/loop.py
@@ -39,7 +39,7 @@ import random
 #     # 3
 #     if capital.lower().strip() == 'q':
 #         break
-#     elif capital.lower().strip() == state.get('capital').lower().strip():
+#    elif capital.lower().strip() == state.get('capital').lower().strip():
 #         # 4
 #         print("Correct")
 #     else:
@@ -48,34 +48,41 @@ import random
 # WHILE IMPLEMENTATION
 # 1
 states = [
-    {"name":"Kaduna", "capital":"Kaduna"},
-    {"name":"Kano", "capital":"Kano"},
+    {"name":"Jigawa", "capital":"Dutse"},
+    {"name":"Kebbi", "capital":"Birnin kebbi"},
     {"name":"Oyo", "capital":"Ibadan"},
 ]
-# 1.2
+
 print("\n\nWelcome to the state and capital game")
 print("To quit, enter 'Q' or 'q' at anytime\n\n")
-#  
-
+print()
+  
+scores=0
 playing = True
 usedStates = []
 while playing:
     state = random.choice(states)
+
     if (state.get('name') in usedStates) == False:
         capital = input("What is the capital of {}: ".format(state.get('name')))
-        # 3
+        
         if capital.lower().strip() == 'q':
             playing = False
         elif capital.lower().strip() == state.get('capital').lower().strip():
-            # 4
-            print("Correct")
+            
+            print("Correct you earned 5 points" )
+            scores += 5
+    
             usedStates.append(state.get('name'))
         else:
             print("Thats wrong, the capital of {} is {}".format(state.get('name'), state.get('capital')))
+            scores= -3   
     else:
         if len(states) == len(usedStates):
             playing = False
             print("Goodbye, you've exhausted our levels")
+        else:
+            
 
 # Q2. Write a State and Capital Game where users are presented with a state
 # for which they provide the capital, if they are correct the program
