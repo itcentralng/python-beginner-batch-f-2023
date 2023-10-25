15e6eba6ba741a6174ddae9c2cff43813cf386fd
nabusani <144116907+nabusani@users.noreply.github.com>
2023-10-25 02:35:22 -0700

    revision.py

--- zainab/classwork/classwork4.py
+++ zainab/classwork/classwork4.py
@@ -1,5 +1,16 @@
-import random 
+#import random 
 
-statesandcapital = {"adamawa":"yola","bayelsa":"yanagoa","kaduna":"kaduna","kebbi":"birnin kebbi","kogi":"lokoja","kwara":"illorin","lagos":"ikeja"}
+#statesandcapital = {"adamawa":"yola","bayelsa":"yanagoa","kaduna":"kaduna","kebbi":"birnin kebbi","kogi":"lokoja","kwara":"illorin","lagos":"ikeja"}
 #1- open a dictionary and write down the names of the states and capital of any choice.
    
+
+def primarycolours(colours):
+    primary = ["red","green","blue"]
+    matching_colours = []
+    for colour in colours:
+        if colour in primary:
+            matching_colours.append(colour)
+    return matching_colours
+
+print(primarycolours(["red","brown","orange","green","pink","blue"]))
+print(primarycolours(["purple","white","silver","black"]))
